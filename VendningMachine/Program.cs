304d51481921d6197b34b435e07f5030ae832517
SaifKhalifa <khsaif5@gmail.com>
2026-09-21 10:37:51 +0300

    initial project

---
+++ VendningMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VendningMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
